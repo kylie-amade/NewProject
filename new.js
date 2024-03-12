1a138040232e895dbc8e2911accf22a37d54251c
kylie-amade <160512873+kylie-amade@users.noreply.github.com>
2024-03-12 17:43:20 +0300

    improved on device width

--- new.js
+++ new.js
@@ -1,3 +1,9 @@
-function handleSearch(){
-
+function handleSearch(event){
+    event.preventDefault();
+    let searchElement = document.querySelector("#searchInput");
+    let cityElement = document.querySelector("#newCity");
+    cityElement.innerHTML = searchElement.value ;
 }
+
+let searchInputElement = document.querySelector("#theForm");
+searchInputElement.addEventListener("submit", handleSearch);
