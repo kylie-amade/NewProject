11265f758d79e6cc7e7bbfcb18da3a72d6aa0e94
kylie-amade <160512873+kylie-amade@users.noreply.github.com>
2024-03-31 04:44:28 +0300

    update on weather forecast

--- new.js
+++ new.js
@@ -15,9 +15,7 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-  iconElement.innerHTML = `<img src = "${
-    response.data.condition.icon - url
-  }" class="weather-emoji" `;
+  iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-emoji" />`;
   getForecast(response.data.city);
 }
 function formatDate(date) {
